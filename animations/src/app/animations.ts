f4655710a548c3df1b182f272773282526110afc
Reynaldo Quispe Sanchez <reynaldoqs@gmail.com>
2017-08-02 19:04:56 -0400

    quitando algunas cosas

--- animations/src/app/animations.ts
+++
@@ -1,6 +0,0 @@
-import { animation, style, animate } from '@angular/animations';
-
-export var fadeAnimation = animation([
-  style({ opacity: "{{ start }}" }),
-  animate("{{ time }}", style({ opacity: "{{ end }}" }))
-], {params: { time: "1s" , start:0, end:1}})
